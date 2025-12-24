e3175a6c8fb6cf87d76d4008acfe226a41c1059c
Snokie <snokiesnoks@gmail.com>
2025-12-24 11:28:02 -0300

    de-WMing and deltajuning

--- src/components/DynamicTitle.tsx
+++ src/components/DynamicTitle.tsx
@@ -6,13 +6,9 @@ function DynamicTitle() {
 
   react.useEffect(() => {
     if (location.pathname === "/") {
-      document.title = "DELTARUNE · WM Newsletter";
-    } else if (location.pathname === "/june2023") {
-      document.title = "DELTARUNE · WM Newsletter: Issue 1 - June 2023";
-    } else if (location.pathname === "/q12025") {
-      document.title = "DELTARUNE · WM Newsletter: Issue 2 - Q1 2025";
-    } else if (location.pathname === "/december2025") {
-      document.title = "DELTARUNE · WM Newsletter: Issue 3 - December 2025";
+      document.title = "DELTAJUNE INTERNAL NEWSLETTER: CHRISTMAS 2025";
+    } else {
+      document.title = "what are you doing here go back";
     }
   }, [location.pathname]);
 
---
+++ src/pages/Christmas2025.tsx
@@ -0,0 +1,149 @@
+const Summer2025 = () => {
+  return (
+    <section className="flex justify-center">
+      <div className="flex flex-col w-full max-w-[568px] items-center mt-10 px-8 md:px-0">
+        <img src="/assets/deltajune-logo.png" alt='Deltajune' className="w-96"></img>
+        <h1 className="text-deltarune-yellow font-pixel-bold md:text-3xl sm:text-xl text-center">
+          CHRISTMAS 2025 
+        </h1>
+        <h1 className="text-deltarune-yellow font-pixel-bold md:text-3xl sm:text-xl text-center">
+          INTERNAL NEWSLETTER 
+        </h1>
+        <div className="flex flex-col">
+          <p className='mt-10'>This is but one of the Christmas surprises I’ve made this year…stay tuned…</p>
+          <img
+            src="/assets/TeachSmartSoap.png"
+            alt='Smart Soap for Smart Men” with a picture of a bar of soap with wire patterns and an apple logo on it'
+            className="max-h-[175px] self-center"
+          ></img>
+
+          <h2 className="font-pixel-bold text-deltarune-yellow md:text-3xl sm:text-xl text-center mb-10 mt-20">
+            A NEWSLETTER? WHAT? HUH?
+          </h2>
+
+          <p>Yeah I was just gonna make a regular old announcement post but I thought it would be more fun to do it this way instead.</p>
+          <p>Who knows, if this game grows big and strong then one day there might be a demand for a Deltajune newsletter!</p>
+          <p>
+            But for now this is just for y’all, to get all of you up to speed on what my plans are for the direction of this project.
+          </p>
+
+          <h2 className="font-pixel-bold text-deltarune-yellow md:text-3xl sm:text-xl text-center mb-10 mt-20">
+            PROGRESS MADE THIS YEAR
+          </h2>
+          <img
+            src="/assets/YantaOnTheComputer.png"
+            alt='Yanta with her trademark tired expression on a computer setup'
+            className="max-h-[175px] self-center mb-10"
+          ></img>
+
+          <p>As you all know, this year I graduated from college! With one more degree than most people get! But on top of that, I also got accepted into grad school and started teaching myself!</p>
+          <p>It has been a wonderful experience, though unfortunately this year has been quite slow in terms of progress on the game itself.</p>
+          <p>I’ve made good progress with the script, and there has been some amazing art and concepts done by our wonderful artists, but overall I think it has been less progress than I was originally hoping for…</p>
+          <p className="mt-20">But! Now that things have settled, next year I’m really putting on the gas. Getting the script finalized, getting a vertical slice done of ACT I, and putting together a fun trailer for Underevent! (You know, if that still ends up happening, who knows). Which leads me to my next idea…</p>
+        
+          <h2 className="font-pixel-bold text-deltarune-yellow md:text-3xl sm:text-xl text-center mb-10 mt-20">
+            EXPANDING THE TEAM
+          </h2>
+          <img
+            src="/assets/KitKitKitKit&Kit.png"
+            alt='Kit with a clipboard looking over four other Kits, each a different color palette from his original design phase'
+            className="max-h-[200px] self-center mb-10"
+          ></img>
+          <p>I think it would be fun to add a couple more people to our roster! I’ve been keeping an eye on some very wonderful artists and animators to join our ranks, and I have started the process of reaching out to a couple.</p>
+          <p>I’ve also been looking to see if there are any UTDR obsessed programmers that would like to join me and Sea on the gamemaking side of things…fingers crossed.</p>
+          <p>I still want to keep our team small as it has been, but I do think adding more talented people will only make our project better (and also you know, make sure it comes out).</p>
+
+          <h2 className="font-pixel-bold text-deltarune-yellow md:text-3xl sm:text-xl text-center mb-10 mt-20">
+            GOING FORWARD
+          </h2>
+          <img
+            src="/assets/YantaKit&BugLookingForward.png"
+            alt='Bug pointing ahead with Yanta and Kit behind them'
+            className="max-h-[200px] self-center mb-10"
+          ></img>
+          <p>I have been curious for a while about potentially dropping a teaser of sorts for this game and seeing what the general interest level would be, but I’m still in love with the idea of shadow dropping it out of the blue.</p>
+          <p>I love surprising people (as you might have guessed, from this surprise newsletter), and so what brings me the most joy is imagining people playing a game they had no idea was coming and loving it.</p>
+          <p>However, I also don’t want the idea for this game to ever peter out. I love these characters and the world we’ve all had a hand in making, and so I figure there might be a point where I just sort of get tired of working on something in secret.</p>
+          <p>I love surprising people (as you might have guessed, from this surprise newsletter), and so what brings me the most joy is imagining people playing a game they had no idea was coming and loving it.</p>
+          <p>All that to say, depending on where we are and how I feel down the road I may end up making this a more public project.</p>
+          <p>The fandom at large is gonna adore the things you all have made, and I desperately want to show it to them. But I’m gonna channel that desire as much into getting the game does as I can before I consider putting some of it out there before it is done!</p>
+
+          <h2 className="font-pixel-bold text-deltarune-yellow md:text-3xl sm:text-xl text-center mb-10 mt-20">
+            2025 HIGHLIGHTS
+          </h2>
+          <p>2025 has still been an eventful year for the project! We had the wonderful Jinx join our team and provide some absolutely gorgeous environment concept art, which will be made into our tilesets and backgrounds!</p>
+          <img
+            src="/assets/JinxPainting.png"
+            alt='Jinx painting a picture'
+            className="max-h-[175px] self-center mb-10"
+          ></img>
+          <p>Soup has made some great sprite art this year, including one of our first enemy sprites (which I still think came out great). Soup has also done a lot of cool things for his own stuff this year, if you ever want to check out his twitter page *COUGH* @BowlOfTailsSoup *COUGH*, and I look forward to working with him more in 2026!</p>
+          <img
+            src="/assets/SoapDrawing.png"
+            alt='Soup’s fox cat thing drawing a picture of Tails from Sonic with a deep look of concentration'
+            className="max-h-[125px] self-center mb-10"
+          ></img>
+          <p>Sea has a lot of stuff going on irl, but he’s still made time to work on programming stuff for our vertical slice we’ve been working on lately! I’m very thankful to still have such a talented person on this project, and you’ll get to see a lot more of his programming genius later next year. Also he’s working on some banger tracks for the OST so there’s that too</p>
+          <img
+            src="/assets/SeaMonsterPiano.png"
+            alt='Sea monster playing on a piano'
+            className="max-h-[175px] self-center mb-10"
+          ></img>
+          <p>And last but certainly not least, Casey has been absolutely rocking it this year. She’s been making lots of great character designs and illustrations for us all to enjoy, as well as helping me out with the script and figuring out some greater writing tasks. Truly this project would not be as good (or probably exist at all) if not for her.</p>
+          <img
+            src="/assets/BeefcliffThundercrackerCaption.png"
+            alt='Beefcliff cat looking at a new Thundercracker figure with envy, subtitle reading “I don’t need it” like that one spongebob meme'
+            className="max-h-[175px] self-center mb-10"
+          ></img>
+          <p>As for me, I’ve managed to make a lot of progress with the script as well as the general plan and outline for the game.</p>
+          <p>Enough so that we can start working on the vertical slice in full force, and hopefully get it done in the first half of 2026! That is ambitious I know, but I’m gonna put as many hours into the Gamemaker Mines as I can in order to make it happen…</p>
+          <p>I’m also planning a full read through of the ACT I script once it is completed, so stay tuned for that!</p>
+          <p>So what else is there left to talk about? Well…</p>
+
+          <img
+            src="/assets/YantaChristmasBlastCaption.png"
+            alt='Yanta yelling “CHRISTMAS BLAST” with her horns glowing and presents floating around her'
+            className="max-h-[250px] self-center mb-10 mt-30"
+          ></img>
+          <h2 className="font-pixel-bold text-deltarune-yellow md:text-3xl sm:text-xl text-center mb-10">
+            CHRISTMAS “BONUSES”
+          </h2>
+          <p>Merry Christmas!</p>
+          <p>In case you haven’t noticed yet, I gifted all of you something from your steam wishlist this year to thank you all for your hard work and patience.</p>
+          <p>You’ve all earned it, and I hope you all enjoy them!</p>
+          <p className="mt-20">Sidenote they were practically giving shit away on black friday like my god I got like 400$ worth of stuff for maybe 90$.</p>
+          <p>Maybe monopolies are morally good.</p>
+
+          <h2 className="font-pixel-bold text-deltarune-yellow md:text-3xl sm:text-xl text-center mb-10 mt-20">
+            FINAL THOUGHTS
+          </h2>
+          <p>I feel kinda embarrassed looking back at how long ago I started working on this, and wondering now what kind of progress I can expect to make in the next year.</p>
+          <p>However regardless of whether we finish the whole thing, or even ACT I, I have had so much fun just making this with all of you.</p>
+          <p>You’re all such great people to work with, and to be honest I wouldn’t have it any other way.</p>
+          <p>Thank you all again for taking this journey with me, and here’s to an even better year next year!</p>
+          <h2 className="font-pixel-bold text-deltarune-yellow md:text-3xl sm:text-xl text-center mb-10 mt-20">
+            AND…A CHRISTMAS QUEST?
+          </h2>
+          <p>
+            Oh yeah, that last surprise I mentioned earlier. Well, it is free to download here and you can look at it whenever you please. I hope you all enjoy :)
+          </p>
+          <img
+            src="/assets/KitBugYanta&TeachOpeningGifts.png"
+            alt='Bug, Yanta, Kit, and Teach all opening a christmas present'
+            className="max-h-[250px] self-center mb-50"
+          ></img>
+          <p>
+            And a special thanks to Snokie, who did all of the amazing doodles in this newsletter and advised me on how to put it together, as well as Caramel for teaching me a lot about running a fan game team! They’ve been so fun to work with on Wandering Makers (a cool Deltarune fan game project), and I feel much more confident in what I want to be doing going forward because of them.</p>
+          <img
+            src="/assets/BugKrisDapping.png"
+            alt='Kris from Deltarune'
+            className="mt-5 mb-20 self-center max-h-[200px]"
+          ></img>
+        </div>
+      </div>
+        
+    </section>
+  );
+};
+
+export default Summer2025;
